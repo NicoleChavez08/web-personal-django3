62176e9c5a9f2e1b73d049064effb90a3be855ff
NicoleChavez08 <157257305+NicoleChavez08@users.noreply.github.com>
2024-05-17 20:58:33 -0500

    Update views.py

--- webpersonal/core/views.py
+++ webpersonal/core/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+def home(request):
+    return HttpResponse("""
+        <h1>Página en desarrollo</h1>
+        <h2><strong>GRUPO #9:</strong></h2>
+        <ul>
+            <li><strong>Integrantes:</strong></li>
+            <li><strong>CHÁVEZ MACÍAS LOYNI NICOLE</strong></li>
+            <li><strong>PIN CEDEÑO JESÚS SIMÓN</strong></li>
+            <li><strong>GUTIERREZ RIVAS CRISTHIAN EDUARDO</strong></li>
+        </ul>
+    """)
